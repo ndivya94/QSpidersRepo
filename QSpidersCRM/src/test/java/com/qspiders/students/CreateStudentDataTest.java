a92f00810b69009457a86f426eccd218d0377d59
Admin <Admin@DESKTOP-7RM5OTU>
2021-09-08 00:51:15 -0700

    project Commit

---
+++ QSpidersCRM/src/test/java/com/qspiders/students/CreateStudentDataTest.java
@@ -0,0 +1,12 @@
+package com.qspiders.students;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class CreateStudentDataTest {
+
+	@Test
+	public void run() {
+		Reporter.log("I am running succesfully",true);
+	}
+}
